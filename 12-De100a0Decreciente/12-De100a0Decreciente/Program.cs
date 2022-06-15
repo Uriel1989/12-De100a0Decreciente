4595c36b021c9a9bff323cf7cb81b7ae4541cb23
Uriel Guillermo Daniel <45216219+Uriel1989@users.noreply.github.com>
2022-06-15 20:27:58 -0300

    Add files via upload

---
+++ 12-De100a0Decreciente/12-De100a0Decreciente/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _12_De100a0Decreciente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+
+            for (i = 100; i >= 0; i--)
+            {
+                Console.WriteLine("Los numeros de la serie son:" + i);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
